360e44f5028b7f01b34633130c4dd73d082ce663
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2024-04-14 01:23:37 +0200

    New lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,7 @@
 
 //! 
 
-//! Imported from ../Output_file/2024-04-13_18h03m14/
-
-mod cmof_dc_cmof_0 {
-
-}
+//! Imported from ../Output_file/2024-04-14_00h59m20/
 
 mod cmof_di_cmof_0 {
 
@@ -17,6 +13,10 @@ mod cmof_bpmn20_cmof_0 {
 }
 
 mod cmof_bpmndi_cmof_0 {
+
+}
+
+mod cmof_dc_cmof_0 {
 
 }
 

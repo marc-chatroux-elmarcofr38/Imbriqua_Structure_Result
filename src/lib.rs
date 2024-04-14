0cf53a19164d77464a0b0bee1c0ffb32d28277fe
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2024-04-14 23:56:13 +0200

    Edit license

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,9 @@
 
 //! 
 
-//! Imported from ../Output_file/2024-04-14_00h59m20/
+//! Imported from ../Output_file/2024-04-14_23h33m52/
 
-mod cmof_di_cmof_0 {
-
-}
-
-mod cmof_bpmn20_cmof_0 {
+mod cmof_dc_cmof_0 {
 
 }
 
@@ -16,7 +12,11 @@ mod cmof_bpmndi_cmof_0 {
 
 }
 
-mod cmof_dc_cmof_0 {
+mod cmof_bpmn20_cmof_0 {
+
+}
+
+mod cmof_di_cmof_0 {
 
 }
 

e8043605aa75f5d285f7c4dc2ad58e84ab408ee9
marc-chatroux-elmarcofr38 <github.dev@elmarcofr38.net>
2025-06-06 19:34:22 +0200

    Minimal project file list

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,13 @@
 
 //! 
 
-//! Imported from "/home/user/Github/Output_file/2024-11-17_19h06m34/"
+//! Imported from "/home/elmarcofr38/Output_file/2025-06-06_19h33m27/"
 
-pub mod dc;
+/// DI_0 : http://www.omg.org/spec/DD/20100524/DI-XMI
 pub mod di;
+/// DC_0 : http://www.omg.org/spec/DD/20100524/DC-XMI
+pub mod dc;
+/// BPMN20_0 : http://www.omg.org/spec/BPMN/20100524/MODEL-XMI
+pub mod bpmn_20;
+/// BPMNDI_0 : http://www.omg.org/spec/BPMN/20100524/DI-XMI
+pub mod bpmndi;
